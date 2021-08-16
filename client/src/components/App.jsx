40e28208834c768e6d6aa25c9ac9fd55a8570408
Dana Walsh <dana.e.walsh@gmail.com>
2021-08-16 15:14:47 -0700

    Page rendering with proper formatting

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+
 const dataEndPoint = 'https://firebasestorage.googleapis.com/v0/b/mikmak-code-challenge.appspot.com/o/inventory_report.js?alt=media&token=87ee3c73-1031-4974-9912-730f8aad664e';
 
 
@@ -8,6 +9,7 @@ class App extends Component {
     super(props);
     this.state = {
       candyInfo: [],
+      loaded: false,
     }
     this.formatData = this.formatData.bind(this);
   }
@@ -33,7 +35,9 @@ class App extends Component {
 
       // create and add the last info for each candy (inventoryValue)
       let inventoryValue = Math.round(100 * (item.remainingStock * productSpecifics.price))/100;
+      let revenue = Math.round(100 * (item.sold * productSpecifics.price))/100;
       productDetails.inventoryValue = inventoryValue;
+      productDetails.revenue = revenue;
 
       // push each set of candy details to the arrayOfProducts array
       arrayOfProducts.push(productDetails);
@@ -41,17 +45,50 @@ class App extends Component {
 
     // store the data in state
     this.setState({
-      candyInfo: arrayOfProducts
+      candyInfo: arrayOfProducts,
+      loaded: true
     })
   }
 
   render() {
+    const { candyInfo, loaded } = this.state;
     return (
-      <div>
-        Hello World
-      </div>
+      <>
+      {loaded ? (
+       <div>
+         {/* <List candies={candyInfo}/> */}
+         {candyInfo.map((candy) => {
+           return (
+           <div key={candy.sku} style={containerDivStyle}>
+             <img src={candy.image} style={imageStyle}></img>
+            <div style={{height: "100px"}}>
+              <div>{candy.name}</div>
+              <div>Number Sold {candy.sold}</div>
+              <div>Revenue ${candy.revenue}</div>
+              <br/>
+              <div>Outstanding Inventory {candy.remainingStock}</div>
+              <div>Inventory Value ${candy.inventoryValue}</div>
+            </div>
+           </div>)
+
+         })}
+       </div>
+      ) : <div>Candy Information Loading</div>}
+      </>
     );
   }
 }
 
 export default App;
+
+const containerDivStyle = {
+  display: "flex",
+  flexDirection: "row",
+  margin: "20px"
+}
+
+const imageStyle = {
+  height: "100px",
+  objectFit: "contain",
+  margin: "10px"
+}
