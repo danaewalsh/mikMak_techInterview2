fd96155ab608807b38f3f2609ddaaae7e4666bd6
Dana Walsh <dana.e.walsh@gmail.com>
2021-08-16 13:40:07 -0700

    Data pulling correctly from the end point, and formatted into iterable array format

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 const dataEndPoint = 'https://firebasestorage.googleapis.com/v0/b/mikmak-code-challenge.appspot.com/o/inventory_report.js?alt=media&token=87ee3c73-1031-4974-9912-730f8aad664e';
 
 
@@ -6,18 +7,42 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.state = {
-
+      candyInfo: [],
     }
+    this.formatData = this.formatData.bind(this);
   }
 
-  async componentDidMount() {
-    let response = await fetch(dataEndPoint);
-    let data = await response.json();
-    console.log('data', data)
+  componentDidMount() {
+    // pull candy data from the end point
+    axios.get(dataEndPoint)
+    .then(({data}) => {
+      // formatData function creates a more useable array data structure and assigns it to state
+      this.formatData(data.inventory_report, data.product_info);
+    })
   }
 
-  formatData() {
+  formatData(inventoryReport, productInfo) {
+    let arrayOfProducts = [];
+    inventoryReport.forEach((item) => {
+      let sku = item.sku;
+      let productSpecifics = productInfo[sku];
+      let productDetails = {
+        ...productSpecifics,
+        ...item
+      }
 
+      // create and add the last info for each candy (inventoryValue)
+      let inventoryValue = Math.round(100 * (item.remainingStock * productSpecifics.price))/100;
+      productDetails.inventoryValue = inventoryValue;
+
+      // push each set of candy details to the arrayOfProducts array
+      arrayOfProducts.push(productDetails);
+    })
+
+    // store the data in state
+    this.setState({
+      candyInfo: arrayOfProducts
+    })
   }
 
   render() {
